0c11cb71a16429910a6cf2db5b3e22b4e45e6f80
hoaibao173@gmail.com <hoaibao173@gmail.com>
2022-12-12 09:03:06 +0700

    vidu

--- cat_delete.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include './config.php';
-include './function.php';
-spl_autoload_register('loadClass');
-$id=isset($_GET['id'])?$_GET['id']:'';
-$o=new Category();
-$o->delete($id);
-header('location:cat.php');
-?>
